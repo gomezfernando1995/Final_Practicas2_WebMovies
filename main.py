e44bbc1b2263df96b9035d4dc74bb440374b5211
gomezfernando1995 <gomezfernando1995@gmail.com>
2023-02-11 04:18:36 -0300

    Final Terminado

--- main.py
+++ main.py
@@ -14,18 +14,28 @@ def home():
     url_trending= 'https://api.themoviedb.org/3/trending/movie/'+time+KEY+page+LENG
     url_movie= 'https://api.themoviedb.org/3/movie/'
     url_poster= 'https://image.tmdb.org/t/p/w300'
+    url_poster_back= 'https://image.tmdb.org/t/p/w1920_and_h800_multi_faces'
+    url_populary = 'https://api.themoviedb.org/3/discover/movie'+KEY+LENG+'&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate'
     
     moviedb=requests.get(url_trending)      #---Solicitamos toda la informacion con el endpoint de trending.
     moviedb_json = moviedb.json()           #---RESPUESTA EN FORMATO JSON-- Tranformamos en json para manejar la informacion obtenida.
     movies_json = moviedb_json['results']   # Con el datos en json podemos ingresar a la lista de resultados
+
+    moviedb_pop=requests.get(url_populary)      #---Solicitamos toda la informacion con el endpoint de trending.
+    moviedb_json_pop= moviedb_pop.json()           #---RESPUESTA EN FORMATO JSON-- Tranformamos en json para manejar la informacion obtenida.
+    movies_json_pop = moviedb_json_pop['results']   # Con el datos en json podemos ingresar a la lista de resultados
+    video = '/videos'+KEY+LENG+"&append_to_response=videos"
+
     
     if request.method == 'GET': 
-        return render_template('index2.html', movies = movies_json,url_img=url_poster,leng=LENG) ##ver aca
+        return render_template('index.html', movies = movies_json,url_img=url_poster,leng=LENG,movie_pop=movies_json_pop,poster_back=url_poster_back,url_movie=url_movie,video=video) ##ver aca
     if request.form['search']:      
-         return render_template('page_unique.html',movies = movies_json,title=request.form['search'],url_img=url_poster,leng=LENG)
+         return render_template('index.html',movies = movies_json,title=request.form['search'],url_img=url_poster,leng=LENG)
     else:
-        return render_template('index2.html')
+        return render_template('index.html')
       
+
+
 
 if __name__ == '__main__':
      app.run(debug=True, port=4000 )
